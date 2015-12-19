ac709e475da2d5abce4c06939c25f278cbc1e49a
Shubham Utwal <shubhamutwal@gmail.com>
2015-12-19 22:41:37 +0530

    UI Change: Modal added 1. Navbar - bootstrap  modal added 2. Index html file updated with jquery cdn :reference.

--- book-store-root/book-store-web/src/main/app.ts
+++ book-store-root/book-store-web/src/main/app.ts
@@ -3,7 +3,10 @@ import {NavBar} from './shared/navbar/navbar';
 
 @Component({
     selector: 'my-app',
-    template: '<nav-bar></nav-bar>',
+    template: `
+    <nav-bar></nav-bar>
+    
+    `,
     directives: [CORE_DIRECTIVES, FORM_DIRECTIVES, NavBar]
 })
 class AppComponent {
